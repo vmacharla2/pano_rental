e6903ddeeeee806510afc5a8b1343aeced81064a
VinayKumar Macharla <vmacharla2@dxc.com>
2022-04-11 15:31:19 +0530

    first commit

---
+++ src/main/java/com/pano/rental/PanoRentalBeApplication.java
@@ -0,0 +1,13 @@
+package com.pano.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanoRentalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanoRentalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pano/rental/PanoRentalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pano.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanoRentalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
